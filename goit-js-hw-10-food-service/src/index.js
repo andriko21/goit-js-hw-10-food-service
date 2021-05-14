417f31c0e9c3a646025e34964dc87333331349df
andriko21 <75207634+andriko21@users.noreply.github.com>
2021-05-15 00:35:44 +0300

    добавив шаблонізатор та почав робити розмітку

--- goit-js-hw-10-food-service/src/index.js
+++ goit-js-hw-10-food-service/src/index.js
@@ -1,1 +1,11 @@
 import './styles.css';
+import menuFoods from './menu.json'
+import receptTpl from './templates/recept_card.hbs'
+
+const menuRefs = {
+  menu: menuFoods,
+}
+
+const menuMarkup = receptTpl(menuRefs);
+
+document.querySelector('.js-menu').innerHTML = menuMarkup;
