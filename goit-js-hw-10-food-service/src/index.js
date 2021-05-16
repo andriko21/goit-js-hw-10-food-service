f20e60b6b3a670d2798795c5e21829bb2e50b9a1
andriko21 <75207634+andriko21@users.noreply.github.com>
2021-05-16 16:19:30 +0300

    доробив все

--- goit-js-hw-10-food-service/src/index.js
+++ goit-js-hw-10-food-service/src/index.js
@@ -1,6 +1,41 @@
 import './styles.css';
-import menuFoods from './menu.json'
-import receptTpl from './templates/recept_card.hbs'
+import menuFoods from './menu.json';
+import receptTpl from './templates/recept_card.hbs';
 
+document.querySelector('.js-menu').insertAdjacentHTML(
+  'beforeend',
+  menuFoods.map(menuFood => receptTpl(menuFood)).join(''),
+);
 
-document.querySelector('.js-menu').insertAdjacentHTML('beforeend', menuFoods.reduce((acc , card) => acc += receptTpl(card),''));
+const themeRefs = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const checkboxSwitcher = document.querySelector('#theme-switch-toggle');
+const body = document.querySelector('body');
+
+function switcherTheme(event) {
+  body.setAttribute(
+    'class',
+    body.classList.contains(themeRefs.DARK) ? themeRefs.LIGHT : themeRefs.DARK,
+  );
+  // console.log(body.classList.contains(Theme.DARK))
+  localStorage.setItem('theme', body.classList.value);
+}
+
+checkboxSwitcher.addEventListener('change', switcherTheme);
+
+currentTheme();
+
+function currentTheme(event) {
+  body.setAttribute(
+    'class',
+    localStorage.getItem('theme') === null
+      ? themeRefs.LIGHT
+      : localStorage.getItem('theme'),
+  );
+  if (localStorage.getItem('theme') === themeRefs.DARK) {
+    checkboxSwitcher.checked = 'true';
+  }
+}
